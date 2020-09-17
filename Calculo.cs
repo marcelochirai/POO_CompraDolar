0cb61b4509aef722fa6a6571980f221886ca725f
marcelochirai <65429412+marcelochirai@users.noreply.github.com>
2020-09-17 00:18:10 -0300

    Add files via upload
    QUAL É A COTAÇÃO DO DÓLAR? 3.10
    QUANTOS DÓLARES VOCÊ IRÁ COMPRAR? 200.0
    VALOR A SER PAGO EM REAIS = 657.20

---
+++ Calculo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompraDolar
+{
+    class Calculo
+    {
+        public static double cotDolar;
+        public static double qtdDolar;
+        public static double iof = 6.0;
+
+        public static double ValorCompra()
+        {
+            double totalDolar = qtdDolar * cotDolar;    //Valor sem IOF
+            return totalDolar+totalDolar*iof/100.0;     //Valor com IOF
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace CompraDolar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar?  ");
+            Calculo.cotDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você irá comprar?  ");
+            Calculo.qtdDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Valor a ser pago em Reais com IOF: " + Calculo.ValorCompra().ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadKey();
+        }
+    }
+}
